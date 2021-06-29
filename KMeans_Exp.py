bcc7f10c196ba8199adec5afdfc49ab346080521
Karun Shrestha <karunkop@gmail.com>
2021-06-29 21:58:23 +0545

    WIP for image segmentation

---
+++ KMeans_Exp.py
@@ -0,0 +1,37 @@
+from KMeans import KMeans
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+#=-=-=-=-=-=-=-=-=-=-
+#   Generate data
+#=-=-=-=-=-=-=-=-=-=-
+classA = -2 * np.random.rand(40,2).T
+classB = 1 + 2 * np.random.rand(20,2).T
+classC = 3 + 2 * np.random.rand(20,2).T
+X = np.concatenate((classA, classB), axis=1)
+X = np.concatenate((X, classC), axis=1).T
+
+o = np.array([[-2, 2],[1, 3.5],[3, 5]]) #initialize centroids
+
+#=-=-=-=-=-=-=-=-=-=-
+#   Create and train
+#   the model.
+#=-=-=-=-=-=-=-=-=-=-
+
+# Three clusters with predefined intial centroid positions
+km = KMeans(n_clus = 3)
+km.fit(X, init_state = o)
+
+# Two clusters with random initialization
+#km = KMeans(n_clus = 2)
+#km.fit(X)
+
+#=-=-=-=-=-=-=-=-=-=-
+#   Vizualize final
+#   result and predict
+#   clusters.
+#=-=-=-=-=-=-=-=-=-=-
+point_test = np.array([[2,2], [-2,-2]])
+print(km.predict(point_test))
+km.vizualize()
---
+++ k_means_obj.py
@@ -0,0 +1,36 @@
+from os import makedirs
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+
+BLUE = (0,0,255)
+RED = (255,0,0)
+
+image = cv2.imread("r_b.jpg")
+# plt.imshow(image)
+
+# Change color to RGB (from BGR)
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+image = image[300:,:]
+pixel_vals = image.reshape((-1,3))
+pixel_vals = np.float32(pixel_vals)
+
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.2)
+
+k = 2
+retval, labels, centers = cv2.kmeans(pixel_vals, k, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+
+centers = np.uint8(centers)
+segmented_data = centers[labels.flatten()]
+
+segmented_image = segmented_data.reshape((image.shape))
+labels_reshape = labels.reshape(image.shape[0], image.shape[1])
+
+cluster = 2
+
+masked_image = np.copy(image)
+masked_image[labels_reshape == cluster] = [BLUE]
+cv2.imwrite('2_next_obj.png', masked_image)
+# plt.show()
+
+# plt.savefig('2_next_obj.png')
